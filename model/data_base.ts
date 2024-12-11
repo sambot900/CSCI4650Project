a3588043e173ec22dcbec602cc548f92629f6ab9
sambot900 <106410947+sambot900@users.noreply.github.com>
2024-12-10 20:45:21 -0600

    Update data_base.ts

--- model/data_base.ts
+++ model/data_base.ts
@@ -2,6 +2,6 @@ export type User = {
     sport : string;
     teamname: string;
     location: string;
-    totalpointsscored: number;
-    numberofwins: number;
+    totalpointsscored: string;
+    numberofwins: string;
 }
